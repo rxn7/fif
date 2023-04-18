b46601d63fadcab03e5c9deae5658670256431dd
rxn7 <rotthin_dev@protonmail.com>
2023-04-18 21:56:43 +0200

    Dont render Profiler TreeNode when profiling is disabled

--- include/fif/core/profiler.h
+++ include/fif/core/profiler.h
@@ -13,7 +13,7 @@ namespace fif::core {
 	} // namespace Profiler
 } // namespace fif::core
 
-#ifdef FIF_PROFILING_EXPERIMENTAL
+#ifdef FIF_PROFILING_ENABLED
 #define FIF_PROFILE_FUNC() fif::core::ScopeTimer __timer##__LINE__ = fif::core::ScopeTimer(FIF_FUNCTION_NAME, [&](fif::core::TimerResult result) { fif::core::Profiler::addResult(result); })
 #else
 #define FIF_PROFILE_FUNC()
