f5e100bdddd07c0e289127bf9594f7a0ab793062
rxn7 <rotthin_dev@protonmail.com>
2023-04-18 20:54:44 +0200

    Fixed the deploy script

--- editor/deploy.sh
+++ editor/deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf dist/*
 mkdir -p dist
-cp ../build-emsc/editor/fifed.js dist/fifed.js
-cp ../build-emsc/editor/fifed.wasm dist/fifed.wasm
+cp ../build-emsc/editor/fifed.js dist
+cp ../build-emsc/editor/fifed.wasm dist
 cp ../build-emsc/editor/fifed.html dist/index.html
 
 pushd dist
-git add . -f
-git commit -a -m "Update"
-git push origin gh-pages 
+npx gh-pages@3.0.0 --dist .
 popd
