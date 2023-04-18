b3f830a4a8b8956675b256fe273cc7b3f0c5788d
rxn7 <rotthin_dev@protonmail.com>
2023-04-18 20:48:23 +0200

    Remove github workflows

--- editor/deploy.sh
+++ editor/deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 rm -rf dist/*
-cp ../build-emsc/editor/fifed.js dist/
-cp ../build-emsc/editor/fifed.wasm dist/
+mkdir -p dist
+cp ../build-emsc/editor/fifed.js dist/fifed.js
+cp ../build-emsc/editor/fifed.wasm dist/fifed.wasm
 cp ../build-emsc/editor/fifed.html dist/index.html
 
 pushd dist
-git add .
+git add . -f
 git commit -a -m "Update"
-git push origin gh-pages
+git push origin gh-pages 
 popd
