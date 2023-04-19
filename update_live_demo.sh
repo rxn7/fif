5cd99ad05e5220281449af8d0cf7d67e6b396002
rxn7 <rotthin_dev@protonmail.com>
2023-04-19 02:46:24 +0200

    Added cmake presets and updated update_live_demo.sh

--- update_live_demo.sh
+++ update_live_demo.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-emcmake cmake -Bbuild-emsc -GNinja
+emcmake cmake . -Bbuild-emsc -GNinja
 cmake --build build-emsc
 mkdir -p dist
 
-cp build-emsc/editor/fifed.js dist
-cp build-emsc/editor/fifed.wasm dist
-cp build-emsc/editor/fifed.html dist/index.html
+cp build-emsc/fifed/fifed.js dist
+cp build-emsc/fifed/fifed.wasm dist
+cp build-emsc/fifed/fifed.html dist/index.html
 
 pushd dist
 npx gh-pages@3.0.0 --dist .
