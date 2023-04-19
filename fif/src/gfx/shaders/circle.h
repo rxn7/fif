b0dc7bb53dc15fa9ba93749bf0798cc6258fb704
rxn7 <rotthin_dev@protonmail.com>
2023-04-19 02:24:01 +0200

    Optimized the circle shader

--- fif/src/gfx/shaders/circle.h
+++ fif/src/gfx/shaders/circle.h
@@ -3,9 +3,9 @@
 #include <memory>
 
 namespace fif::gfx::priv::shaders {
-    namespace Circle {
-        const std::string FRAGMENT = FIF_GLSL_VERSION FIF_GLSL_PRECISION
-            R"(
+	namespace Circle {
+		const std::string FRAGMENT = FIF_GLSL_VERSION FIF_GLSL_PRECISION
+			R"(
 		in vec4 v_Color;
 		in vec2 v_UV;
 
@@ -13,8 +13,8 @@ namespace fif::gfx::priv::shaders {
 
 		void main() {
 			float dist = distance(vec2(0.5, 0.5), v_UV) * 2.0;
-			float alpha = dist > 0.99 ? 1.0 - smoothstep(0.99, 1.0, dist) : 1.0;
-			f_Color = mix(vec4(0.0), v_Color, alpha);
+			float alpha = 1.0 - smoothstep(v_Color.a * 0.99, v_Color.a, dist);
+			f_Color = vec4(v_Color.xyz, alpha);
 		})";
-    }
+	}
 } // namespace fif::gfx::priv::shaders
