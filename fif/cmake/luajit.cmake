f47fbcb63c48edf24245a8f9a6856394df684da7
rxn7 <rotthin_dev@protonmail.com>
2023-05-04 17:32:54 +0200

    CMake updated

--- fif/cmake/luajit.cmake
+++
@@ -1,7 +0,0 @@
-set(LUAJIT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/vendor/LuaJIT)
-
-file(GLOB_RECURSE LUAJIT_SOURCES ${LUAJIT_PATH}/*.cpp)
-
-add_library("LuaJIT" STATIC ${IMGUI_SOURCES})
-target_include_directories("LuaJIT" PUBLIC ${LUAJIT_PATH}/src)
-set_target_properties("LuaJIT" PROPERTIES LINK_FLAGS "${LFLAGS}")
