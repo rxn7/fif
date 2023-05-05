0bcfbc65e627e280ab95f1e14550f3ace70b7506
rxn7 <rotthin_dev@protonmail.com>
2023-05-05 08:04:22 +0200

    Add QuadComponent or CircleComponent randomly

--- fifed/src/panels/entities/entities_panel.cpp
+++ fifed/src/panels/entities/entities_panel.cpp
@@ -1,5 +1,6 @@
 #include "entities_panel.hpp"
 
+#include "components/circle_component.hpp"
 #include "fif/gfx/components/quad_component.hpp"
 #include "fif/gfx/components/transform_component.hpp"
 
@@ -20,8 +21,13 @@ namespace fifed {
 			TransformComponent &trans = scene.add_component<TransformComponent>(ent);
 			trans.position = Rng::get_vec2(-1000, 1000);
 
-			QuadComponent &quad = scene.add_component<QuadComponent>(ent);
-			quad.size = glm::vec2(Rng::get_f32(20, 100), Rng::get_f32(20, 100));
+			if(Rng::get_bool()) {
+				QuadComponent &quad = scene.add_component<QuadComponent>(ent);
+				quad.size = glm::vec2(Rng::get_f32(20, 100), Rng::get_f32(20, 100));
+			} else {
+				CircleComponent &circle = scene.add_component<CircleComponent>(ent);
+				circle.radius = Rng::get_f32(40, 200);
+			}
 
 			RenderableComponent &renderable = scene.add_component<RenderableComponent>(ent);
 			renderable.color = Color(Rng::get_u8(0u, 255u), Rng::get_u8(0u, 255u), Rng::get_u8(0u, 255u), Rng::get_u8(100u, 255u));
