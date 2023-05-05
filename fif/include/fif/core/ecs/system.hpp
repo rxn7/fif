16c62cc35ed2fd4f864de13c18ee394896a80bd9
rxn7 <rotthin_dev@protonmail.com>
2023-05-05 05:11:30 +0200

    Removed unnecessary include

--- fif/include/fif/core/ecs/system.hpp
+++ fif/include/fif/core/ecs/system.hpp
@@ -2,8 +2,6 @@
 
 #include "fif/core/event/event.hpp"
 
-#include "entt/entt.hpp"
-
 namespace fif::core {
 	typedef std::function<void(entt::registry &registry)> RenderSystem;
 	typedef std::function<void(entt::registry &registry, f32 dt)> UpdateSystem;
