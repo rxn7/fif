fb696bcb4149992e085c58fd5d19aedda23b2121
rxn7 <rotthin_dev@protonmail.com>
2023-05-10 05:24:39 +0200

    Cross compiling to windows

--- fif/include/fif/core/util/logger.hpp
+++ fif/include/fif/core/util/logger.hpp
@@ -10,8 +10,10 @@ namespace fif::core {
 		static void add_callback(LoggerCallback callback);
 
 		template<typename... Args> static void info(const char *msg, Args... args) {
-			char *buf;
-			asprintf(&buf, msg, std::forward<Args>(args)...);
+			size_t size = snprintf(NULL, 0, msg, std::forward<Args>(args)...) + 1;
+
+			char *buf = static_cast<char *>(std::malloc(size));
+			snprintf(buf, size, msg, std::forward<Args>(args)...);
 
 			for(LoggerCallback &callback : s_Callbacks)
 				callback(LogType::INFO, buf);
@@ -23,8 +25,10 @@ namespace fif::core {
 		}
 
 		template<typename... Args> static void warn(const char *msg, Args... args) {
-			char *buf;
-			asprintf(&buf, msg, std::forward<Args>(args)...);
+			size_t size = snprintf(NULL, 0, msg, std::forward<Args>(args)...) + 1;
+
+			char *buf = static_cast<char *>(std::malloc(size));
+			snprintf(buf, size, msg, std::forward<Args>(args)...);
 
 			for(LoggerCallback &callback : s_Callbacks)
 				callback(LogType::WARN, buf);
@@ -36,8 +40,10 @@ namespace fif::core {
 		}
 
 		template<typename... Args> static void error(const char *msg, Args... args) {
-			char *buf;
-			asprintf(&buf, msg, std::forward<Args>(args)...);
+			size_t size = snprintf(NULL, 0, msg, std::forward<Args>(args)...) + 1;
+
+			char *buf = static_cast<char *>(std::malloc(size));
+			snprintf(buf, size, msg, std::forward<Args>(args)...);
 
 			for(auto &callback : s_Callbacks)
 				callback(LogType::ERROR, buf);
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,7 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd ${SCRIPT_DIR}/.. >/dev/null
 
-cmake . -Bbuild/debug -GNinja -DCMAKE_BUILD_TYPE=Debug
-cmake . -Bbuild/release -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake . -Bbuild/debug -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
+cmake . -Bbuild/release -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+
+x86_64-w64-mingw32-cmake . -Bbuild/mingw/debug -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
+x86_64-w64-mingw32-cmake . -Bbuild/mingw/release -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
 
 popd >/dev/null
