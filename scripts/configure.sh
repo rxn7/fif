544b2c45ba8774c932f21955a7fa715404dd37c0
rxn7 <rotthin_dev@protonmail.com>
2023-05-10 05:49:18 +0200

    Configure windows build only if mingw cmake is present

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -6,7 +6,9 @@ pushd ${SCRIPT_DIR}/.. >/dev/null
 cmake . -Bbuild/debug -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
 cmake . -Bbuild/release -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 
-x86_64-w64-mingw32-cmake . -Bbuild/mingw/debug -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
-x86_64-w64-mingw32-cmake . -Bbuild/mingw/release -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
+if command -v x86_64-w64-mingw32-cmake &> /dev/null ; then
+	x86_64-w64-mingw32-cmake . -Bbuild/mingw/debug -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
+	x86_64-w64-mingw32-cmake . -Bbuild/mingw/release -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE
+fi
 
 popd >/dev/null
