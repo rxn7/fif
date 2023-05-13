66d6e4e196a5e05978a4d3b670946243d085b1d7
rxn7 <rotthin_dev@protonmail.com>
2023-05-13 09:32:09 +0200

    Profile release build instead of debug

--- scripts/profiler.sh
+++ scripts/profiler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd ${SCRIPT_DIR}/../build/debug/fifed >/dev/null
+pushd ${SCRIPT_DIR}/../build/release/fifed >/dev/null
 valgrind --tool=callgrind ./fifed
 qcachegrind 
 popd
